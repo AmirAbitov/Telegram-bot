04c00c9e60086a372c5a7cfe878519df96ce1421
AmirAbitov <118526634+AmirAbitov@users.noreply.github.com>
2022-12-05 22:14:57 +0500

    Id

--- bot.mjs
+++ bot.mjs
@@ -3,8 +3,8 @@ import TeleBot from "telebot"
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on('/start',(msg) => {
-     bot.sendSticker(msg.from.id,`https://tlgrm.eu/_/stickers/375/374/37537478-18f8-4d53-9dc4-99508f460f18/192/5.webp`)
-     return bot.sendMessage(msg.from.id, `Salom, ${msg.from.first_name} Amir yaratgan botga xush kelipsiz!`)
+     bot.sendSticker(chatId,`https://tlgrm.eu/_/stickers/375/374/37537478-18f8-4d53-9dc4-99508f460f18/192/5.webp`)
+     return bot.sendMessage(chatId, `Salom, ${msg.from.first_name} Amir yaratgan botga xush kelipsiz!`)
 })
 
 export default bot
