3c628f871e6b71f4ea743c1a06eff1cd887c30e6
AmirAbitov <118526634+AmirAbitov@users.noreply.github.com>
2022-12-05 21:59:49 +0500

    start

--- bot.mjs
+++ bot.mjs
@@ -4,7 +4,7 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on('/start',(msg) => {
      bot.sendSticker(msg.from.id,`https://tlgrm.eu/_/stickers/375/374/37537478-18f8-4d53-9dc4-99508f460f18/192/5.webp`)
-     bot.sendMessage(msg.from.id, `Salom, ${msg.from.first_name} Amir yaratgan botga xush kelipsiz!`)
+     return bot.sendMessage(msg.from.id, `Salom, ${msg.from.first_name} Amir yaratgan botga xush kelipsiz!`)
 })
 
 export default bot
